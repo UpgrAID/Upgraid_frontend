b0e59818773f1433b1bda051eba5cdc64f9eb516
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-12-07 10:37:57 -0800

    initial commit

---
+++ js/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('../main.css');
+var Test = require('../components/loginRegistration/test.jsx');
+ReactDOM.render(<Test/>, document.getElementById('test'));
