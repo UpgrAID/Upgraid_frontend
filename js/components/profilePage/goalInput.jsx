90d8b539ba4decf1ecdf20a2d012b843baa9a688
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-12-08 16:11:36 -0800

    added model save to goal

--- js/components/profilePage/goalInput.jsx
+++ js/components/profilePage/goalInput.jsx
@@ -3,6 +3,22 @@ var React = require('react');
 var GoalInput = React.createClass({
 	_submit: function(e) {
 		e.preventDefault();
+		var Goal = Backbone.Model.extend({
+		url:'https://safe-brook-9891.herokuapp.com/api/goals/',
+		initialize: function() {
+		console.log('Making Goal.')
+		}	
+	});
+
+		var test = new Goal();
+		test.set({
+			title:$('#goalInput').val()
+		})
+		test.save({}, {
+			success: function(resp) {
+				console.log(resp);
+			}
+		})
 },
 	render: function() {
 		return(
