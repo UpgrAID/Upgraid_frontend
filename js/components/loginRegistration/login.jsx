4f502ea6e56b074d4ae7e909c440078bcd2ac944
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2016-01-05 16:22:11 -0800

    added styling

--- js/components/loginRegistration/login.jsx
+++ js/components/loginRegistration/login.jsx
@@ -51,10 +51,10 @@ var Login = React.createClass({
 						<form id="loginForm" method='POST' onSubmit={this._submit}>
 							<div id="inputContainer">
 								<div className="inputWindow">
-									<input id="username" className="loginInput"  placeholder="username" value="thomas1117"/>
+									<input id="username" className="loginInput"  placeholder="username" />
 								</div>
 								<div className="inputWindow">
-									<input id="password" className="loginInput"  placeholder="password" type="password" value="qqq"/>
+									<input id="password" className="loginInput"  placeholder="password" type="password" />
 								</div>
 								<button id="submitLog" type="submit">Submit</button>
 								
