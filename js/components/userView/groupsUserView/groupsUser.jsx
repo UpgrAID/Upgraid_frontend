8bc3df40c42d3bbc51c3993de3b2b9858c0c1afb
notewrangler <noiseartist@gmail.com>
2016-01-08 09:57:28 -0800

    added an apostrophe to groupsUser

--- js/components/userView/groupsUserView/groupsUser.jsx
+++ js/components/userView/groupsUserView/groupsUser.jsx
@@ -3,13 +3,13 @@ var GroupsUserBtn = require('./groupsUserBtn.jsx');
 var GroupsUser = React.createClass({
 	render: function(){
 		var props = this.props;
-		
+
 		var test=this.props.groupsUser.map(function(obj){
 
 					return(<p key={obj.id} className="groupUserButton"><GroupsUserBtn router={props.router} theme={obj.theme} id={obj.group} title = {obj.title} completed={obj.completed}/></p>)
 
 			})
-					return(<div id="groupsUserContainer"><h2 id="groupsUserHeader">{this.props.theirName}s Groups</h2>{test}</div>)
+					return(<div id="groupsUserContainer"><h2 id="groupsUserHeader">{this.props.theirName}'s Groups</h2>{test}</div>)
 			}
 		});
 
